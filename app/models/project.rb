e3015ad2b82a097e1380cce62223be6821ec8415
neonmate <neonmate@users.noreply.github.com>
2019-03-10 18:53:04 +0100

    Update rubocop config

--- app/models/project.rb
+++ app/models/project.rb
@@ -8,7 +8,7 @@ class Project < ApplicationRecord
   belongs_to :user
 
   validates :title, :subtitle, :description, :repository_url, presence: true
-  validates :repository_url, format: {with: REPOSITORY_URL_PATTERN}
-  validates :application_url, format: {with: APPLICATION_URL_PATTERN, allow_blank: true}
+  validates :repository_url, format: { with: REPOSITORY_URL_PATTERN }
+  validates :application_url, format: { with: APPLICATION_URL_PATTERN, allow_blank: true }
 
 end

315c68ee158459ee83f363e400a398f4a198754d
neonmate <neonmate@users.noreply.github.com>
2019-11-18 21:02:35 +0100

    Adjust spacing

--- app/inputs/text_input.rb
+++ app/inputs/text_input.rb
@@ -1,7 +1,7 @@
 class TextInput < SimpleForm::Inputs::TextInput
-    def input(*)
-      input_html_options.reverse_merge!(rows: 10)
+  def input(*)
+    input_html_options.reverse_merge!(rows: 10)
 
-      super
-    end
+    super
+  end
 end
